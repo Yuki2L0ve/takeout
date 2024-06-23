2a2933799d3e7a5205da701e5206359280f0784b
Camellia <1179712104@qq.com>
2024-06-23 14:31:50 +0800

    修改菜品功能代码完成

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -18,6 +18,7 @@ import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -108,5 +109,53 @@ public class DishServiceImpl implements DishService {
 
         // 逻辑4：删除菜品表所关联的菜品口味表中的数据   delete from dish_flavor where dish_id in (?, ?, ?)
         dishFlavorMapper.deleteByIds(ids);
+    }
+
+    /**
+     * 根据id查询菜品和对应的口味数据
+     * @param id
+     * @return
+     */
+    @Override
+    public DishVO getByIdWithFlavor(Long id) {
+        // 根据id查询菜品数据
+        Dish dish = dishMapper.getById(id);
+
+        // 根据菜品id查询口味数据
+        List<DishFlavor> dishFlavors = dishFlavorMapper.getByDishId(id);
+
+        // 将查询到的数据封装到DishVO中
+        DishVO dishVO = new DishVO();
+        BeanUtils.copyProperties(dish, dishVO);
+        dishVO.setFlavors(dishFlavors);
+
+        return dishVO;
+    }
+
+    /**
+     * 根据菜品id来修改菜品基本信息和对应的菜品口味信息
+     * @param dishDTO
+     */
+    @Override
+    @Transactional
+    public void updateWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+        // 修改菜品表的基本信息
+        dishMapper.update(dish);
+
+        // 修改菜品口味表的基本信息：分为以下两步，先删除再插入
+        // (1)先删除原有的菜品口味数据
+        dishFlavorMapper.deleteByDishId(dishDTO.getId());
+        // (2)再插入新的菜品口味数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null && flavors.size() > 0) {
+            // 给菜品口味表中每种口味都设置dishId
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishDTO.getId());
+            });
+            // 向菜品口味表插入n条数据
+            dishFlavorMapper.insertBatch(flavors);
+        }
     }
 }
