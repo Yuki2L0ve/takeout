850ce71a765bc31495553458b5ea35d6c8536435
lzj <linzujie1@yonyou.com>
2024-07-23 00:06:40 +0800

    查询购物车功能代码完成

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -63,4 +63,16 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCartMapper.insert(shoppingCart);
         }
     }
+
+    /**
+     * 查看购物车
+     * @return
+     */
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                .userId(BaseContext.getCurrentId())
+                .build();
+        return shoppingCartMapper.list(shoppingCart);
+    }
 }
