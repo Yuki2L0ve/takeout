c2c0245b913cbdf1cf85b9eb4cbf334e171a505a
lzj <linzujie1@yonyou.com>
2024-07-23 00:11:22 +0800

    清空购物车功能代码完成

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -75,4 +75,12 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
                 .build();
         return shoppingCartMapper.list(shoppingCart);
     }
+
+    /**
+     * 清空购物车商品
+     */
+    @Override
+    public void cleanShoppingCart() {
+        shoppingCartMapper.deleteByUserId(BaseContext.getCurrentId());
+    }
 }
